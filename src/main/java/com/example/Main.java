2cfd8f2b302b00e62a4a1762c49e43f5d45d549d
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 09:22:33 +0200

    funge

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,9 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -9,5 +12,17 @@ public class Main {
         System.out.println("Client partito!");
         Socket s = new Socket("localhost", 3000);
         System.out.println("client collegato!");
+
+        //first try 
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        String stringaInviata = "salve" + '\n';
+        out.writeBytes(stringaInviata);
+
+        String stringaRimandata = in.readLine();
+        System.out.println("la stringa mo è: " + stringaRimandata);
+        
+        s.close();
     }
 }
